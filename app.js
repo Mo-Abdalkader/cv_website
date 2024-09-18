4eaae61e5d5d558dc0b943de572e56a790249b9d
Mohamed Abdalkader <101468654+Mo-Abdalkader@users.noreply.github.com>
2024-09-18 13:55:07 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('previewBtn').addEventListener('click', function() {
-    // Get resume input from textarea
-    const resumeText = document.getElementById('resumeInput').value;
-
-    // Display resume input as preview
-    const resumePreview = document.getElementById('resumePreview');
-    resumePreview.textContent = resumeText;
-});
